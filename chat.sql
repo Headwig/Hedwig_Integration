f045c4a31117c409c188f314b5c7968df4fc9eab
Keerat666 <gurkeeratsondhi@gmail.com>
2017-08-07 04:32:31 +0530

    Add files via upload

--- chat.sql
+++ chat.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2017 at 10:34 PM
+-- Generation Time: Jul 26, 2017 at 02:14 PM
 -- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -42,28 +42,13 @@ CREATE TABLE `chat` (
 --
 
 INSERT INTO `chat` (`id`, `sender`, `message`, `receiver`, `chatid`, `status`, `eb`, `date`) VALUES
-(161, 'Germany', 'mbvjf', 'India', 'GermanyIndia', 'r', 'true', '2017-08-06 16:52:06'),
-(162, 'Germany', 'lcodlxs', 'India', 'GermanyIndia', 'r', 'false', '2017-08-06 16:52:06'),
+(161, 'Germany', 'mbvjf', 'India', 'GermanyIndia', 'u', 'true', '2017-07-24 21:56:32'),
+(162, 'Germany', 'lcodlxs', 'India', 'GermanyIndia', 'u', 'false', '2017-07-24 21:56:56'),
 (163, 'Germany', 'lol', 'Eb', 'GermanyEb', 'u', 'true', '2017-07-24 21:59:21'),
-(164, 'Japan', 'Hh', 'Germany', 'JapanGermany', 'r', 'false', '2017-08-06 18:00:23'),
-(165, 'Germany', 'Hi', 'Japan', 'GermanyJapan', 'r', 'true', '2017-08-06 17:28:35'),
-(166, 'Germany', 'lol', 'Japan', 'GermanyJapan', 'r', 'false', '2017-08-06 17:28:35'),
-(167, 'Japan', 'Gaand mara', 'Germany', 'JapanGermany', 'r', 'false', '2017-08-06 18:00:23'),
-(168, 'India', 'Messages are entered here.22:30', 'Germany', 'IndiaGermany', 'r', 'false', '2017-08-06 17:56:12'),
-(169, 'India', 'Messages are entered here.', 'Germany', 'IndiaGermany', 'r', 'false', '2017-08-06 17:56:12'),
-(170, 'Germany', 'Yo bitches', 'India', 'GermanyIndia', 'r', 'false', '2017-08-06 17:57:46'),
-(171, 'Germany', 'Fuck off plis', 'India', 'GermanyIndia', 'r', 'false', '2017-08-06 18:23:10'),
-(172, 'China', 'India', 'India', 'ChinaIndia', 'r', 'false', '2017-08-06 18:35:39'),
-(173, 'China', 'Bhai', 'India', 'ChinaIndia', 'r', 'false', '2017-08-06 18:35:51'),
-(174, 'Germany', 'Fuck off plis', 'China', 'GermanyChina', 'r', 'false', '2017-08-06 18:38:04'),
-(175, 'China', 'Ok', 'Germany', 'ChinaGermany', 'r', 'false', '2017-08-06 18:38:54'),
-(176, 'China', 'ih', 'Germany', 'ChinaGermany', 'r', 'false', '2017-08-06 18:41:09'),
-(177, 'Germany', 'Fishermen', 'China', 'GermanyChina', 'r', 'false', '2017-08-06 18:42:43'),
-(178, 'China', 'asasasasasas', 'Germany', 'ChinaGermany', 'r', 'false', '2017-08-06 18:49:04'),
-(179, 'Germany', 'Bye', 'China', 'GermanyChina', 'r', 'false', '2017-08-06 19:17:21'),
-(180, 'India', 'Hshehe', 'China', 'IndiaChina', 'r', 'false', '2017-08-06 19:17:19'),
-(181, 'India', 'Shdjjd', 'China', 'IndiaChina', 'r', 'false', '2017-08-06 19:18:33'),
-(182, 'India', 'Duhebsidnd', 'China', 'IndiaChina', 'r', 'false', '2017-08-06 19:30:53');
+(164, 'Japan', 'Hh', 'Germany', 'JapanGermany', 'u', 'false', '2017-07-25 02:57:03'),
+(165, 'Germany', 'Hi', 'Japan', 'GermanyJapan', 'u', 'true', '2017-07-25 02:57:06'),
+(166, 'Germany', 'lol', 'Japan', 'GermanyJapan', 'u', 'false', '2017-07-25 02:57:36'),
+(167, 'Japan', 'Gaand mara', 'Germany', 'JapanGermany', 'u', 'false', '2017-07-25 02:58:21');
 
 --
 -- Indexes for dumped tables
@@ -83,7 +68,7 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=168;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
